05680314e0eac626b3beb479425cc418ea3a533d
zacksleo <zacksleo@gmail.com>
2018-04-15 03:24:54 +0800

    feat(grid): add gridview

---
+++ src/Yii.php
@@ -0,0 +1,239 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace zacksleo\laravel\yii;
+
+use InvalidArgumentException;
+use InvalidArgumentException as InvalidConfigException;
+use InvalidArgumentException as InvalidParamException;
+use InvalidArgumentException as UnknownClassException;
+use zacksleo\laravel\yii\di\Container;
+
+/**
+ * BaseYii is the core helper class for the Yii framework.
+ *
+ * Do not use BaseYii directly. Instead, use its child class [[\Yii]] which you can replace to
+ * customize methods of BaseYii.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Yii
+{
+    /**
+     * @var array class map used by the Yii autoloading mechanism.
+     * The array keys are the class names (without leading backslashes), and the array values
+     * are the corresponding class file paths (or [path aliases](guide:concept-aliases)). This property mainly affects
+     * how [[autoload()]] works.
+     * @see autoload()
+     */
+    public static $classMap = [];
+    /**
+     * @var \yii\console\Application|\yii\web\Application the application instance
+     */
+    public static $app;
+    /**
+     * @var array registered path aliases
+     * @see getAlias()
+     * @see setAlias()
+     */
+    public static $aliases = ['@yii' => __DIR__];
+    /**
+     * @var Container the dependency injection (DI) container used by [[createObject()]].
+     * You may use [[Container::set()]] to set up the needed dependencies of classes and
+     * their initial property values.
+     * @see createObject()
+     * @see Container
+     */
+    public static $container;
+
+
+    /**
+     * Returns a string representing the current version of the Yii framework.
+     * @return string the version of Yii framework
+     */
+    public static function getVersion()
+    {
+        return '2.0.12';
+    }
+
+    /**
+     * Translates a path alias into an actual path.
+     *
+     * The translation is done according to the following procedure:
+     *
+     * 1. If the given alias does not start with '@', it is returned back without change;
+     * 2. Otherwise, look for the longest registered alias that matches the beginning part
+     *    of the given alias. If it exists, replace the matching part of the given alias with
+     *    the corresponding registered path.
+     * 3. Throw an exception or return false, depending on the `$throwException` parameter.
+     *
+     * For example, by default '@yii' is registered as the alias to the Yii framework directory,
+     * say '/path/to/yii'. The alias '@yii/web' would then be translated into '/path/to/yii/web'.
+     *
+     * If you have registered two aliases '@foo' and '@foo/bar'. Then translating '@foo/bar/config'
+     * would replace the part '@foo/bar' (instead of '@foo') with the corresponding registered path.
+     * This is because the longest alias takes precedence.
+     *
+     * However, if the alias to be translated is '@foo/barbar/config', then '@foo' will be replaced
+     * instead of '@foo/bar', because '/' serves as the boundary character.
+     *
+     * Note, this method does not check if the returned path exists or not.
+     *
+     * See the [guide article on aliases](guide:concept-aliases) for more information.
+     *
+     * @param string $alias the alias to be translated.
+     * @param bool $throwException whether to throw an exception if the given alias is invalid.
+     * If this is false and an invalid alias is given, false will be returned by this method.
+     * @return string|bool the path corresponding to the alias, false if the root alias is not previously registered.
+     * @throws InvalidParamException if the alias is invalid while $throwException is true.
+     * @see setAlias()
+     */
+    public static function getAlias($alias, $throwException = true)
+    {
+        if (strncmp($alias, '@', 1)) {
+            // not an alias
+            return $alias;
+        }
+
+        $pos = strpos($alias, '/');
+        $root = $pos === false ? $alias : substr($alias, 0, $pos);
+
+        if (isset(static::$aliases[$root])) {
+            if (is_string(static::$aliases[$root])) {
+                return $pos === false ? static::$aliases[$root] : static::$aliases[$root] . substr($alias, $pos);
+            }
+
+            foreach (static::$aliases[$root] as $name => $path) {
+                if (strpos($alias . '/', $name . '/') === 0) {
+                    return $path . substr($alias, strlen($name));
+                }
+            }
+        }
+
+        if ($throwException) {
+            throw new InvalidArgumentException("Invalid path alias: $alias");
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the root alias part of a given alias.
+     * A root alias is an alias that has been registered via [[setAlias()]] previously.
+     * If a given alias matches multiple root aliases, the longest one will be returned.
+     * @param string $alias the alias
+     * @return string|bool the root alias, or false if no root alias is found
+     */
+    public static function getRootAlias($alias)
+    {
+        $pos = strpos($alias, '/');
+        $root = $pos === false ? $alias : substr($alias, 0, $pos);
+
+        if (isset(static::$aliases[$root])) {
+            if (is_string(static::$aliases[$root])) {
+                return $root;
+            }
+
+            foreach (static::$aliases[$root] as $name => $path) {
+                if (strpos($alias . '/', $name . '/') === 0) {
+                    return $name;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Creates a new object using the given configuration.
+     *
+     * You may view this method as an enhanced version of the `new` operator.
+     * The method supports creating an object based on a class name, a configuration array or
+     * an anonymous function.
+     *
+     * Below are some usage examples:
+     *
+     * ```php
+     * // create an object using a class name
+     * $object = Yii::createObject('yii\db\Connection');
+     *
+     * // create an object using a configuration array
+     * $object = Yii::createObject([
+     *     'class' => 'yii\db\Connection',
+     *     'dsn' => 'mysql:host=127.0.0.1;dbname=demo',
+     *     'username' => 'root',
+     *     'password' => '',
+     *     'charset' => 'utf8',
+     * ]);
+     *
+     * // create an object with two constructor parameters
+     * $object = \Yii::createObject('MyClass', [$param1, $param2]);
+     * ```
+     *
+     * Using [[\yii\di\Container|dependency injection container]], this method can also identify
+     * dependent objects, instantiate them and inject them into the newly created object.
+     *
+     * @param string|array|callable $type the object type. This can be specified in one of the following forms:
+     *
+     * - a string: representing the class name of the object to be created
+     * - a configuration array: the array must contain a `class` element which is treated as the object class,
+     *   and the rest of the name-value pairs will be used to initialize the corresponding object properties
+     * - a PHP callable: either an anonymous function or an array representing a class method (`[$class or $object, $method]`).
+     *   The callable should return a new instance of the object being created.
+     *
+     * @param array $params the constructor parameters
+     * @return object the created object
+     * @throws InvalidArgumentException if the configuration is invalid.
+     * @see \yii\di\Container
+     */
+    public static function createObject($type, array $params = [])
+    {
+        self::$container = new Container();
+        if (is_string($type)) {
+            return static::$container->get($type, $params);
+        } elseif (is_array($type) && isset($type['class'])) {
+            $class = $type['class'];
+            unset($type['class']);
+            return static::$container->get($class, $params, $type);
+        } elseif (is_callable($type, true)) {
+            return static::$container->invoke($type, $params);
+        } elseif (is_array($type)) {
+            throw new InvalidArgumentException("Object configuration must be an array containing a \"class\" element.");
+        }
+
+        throw new InvalidArgumentException('Unsupported configuration type: ' . gettype($type));
+    }
+
+    /**
+     * Configures an object with the initial property values.
+     * @param object $object the object to be configured
+     * @param array $properties the property initial values given in terms of name-value pairs.
+     * @return object the object itself
+     */
+    public static function configure($object, $properties)
+    {
+        foreach ($properties as $name => $value) {
+            $object->$name = $value;
+        }
+
+        return $object;
+    }
+
+    /**
+     * Returns the public member variables of an object.
+     * This method is provided such that we can get the public member variables of an object.
+     * It is different from "get_object_vars()" because the latter will return private
+     * and protected variables if it is called within the object itself.
+     * @param object $object the object to be handled
+     * @return array the public member variables of the object
+     */
+    public static function getObjectVars($object)
+    {
+        return get_object_vars($object);
+    }
+}
